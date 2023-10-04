59836117a830eae6aabb022b66130bb42f3b5260
Alfredo Tochon <artochq@gmail.com>
2023-10-04 16:09:36 -0500

    Initial Commit

---
+++ LOTRconverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRconverter
+//
+//  Created by Alfredo Tochon on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRconverter/LOTRconverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRconverterApp.swift
+//  LOTRconverter
+//
+//  Created by Alfredo Tochon on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTRconverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
