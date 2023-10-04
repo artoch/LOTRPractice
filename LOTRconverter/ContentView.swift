c1589604e86df8e2a71b4715434b00c38a9fd23b
Alfredo Tochon <artochq@gmail.com>
2023-10-04 16:12:40 -0500

    add test test

--- LOTRconverter/ContentView.swift
+++ LOTRconverter/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack {//THIS/
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
